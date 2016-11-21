d8736ee31865503433c04738801021579b50b5aa
Rick24 <Rick24@users.noreply.github.com>
2016-11-21 16:40:45 +0000

    Update Basic Q 1 Week 1.py

--- Basic Q 1 Week 1.py
+++ Basic Q 1 Week 1.py
@@ -11,4 +11,4 @@ for i in range(0, 7):  #Loop from 0 to array length
 	if y == i: #If it is the same create it again
 		continue 
 	array[i], array[y] = array[y], array[i] #Replace temp list with actual array
-print(array)
+print(array
